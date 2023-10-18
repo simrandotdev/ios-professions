31a73b97391f3d84289d9577a4f0f78622a12b91
Simran <simran5590@gmail.com>
2023-10-18 16:14:29 -0700

    Project created

---
+++ Bankey/Bankey/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Bankey
+//
+//  Created by Simran Preet Singh Narang on 2023-10-18.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
