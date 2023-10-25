36aaf00c6ebdd3621ef04e8b4f2d522ff914d757
Simran <simran5590@gmail.com>
2023-10-25 07:26:48 -0700

    Added TabBar

---
+++ Bankey/Bankey/MainViewController.swift
@@ -0,0 +1,62 @@
+//
+//  MainViewController.swift
+//  Bankey
+//
+//  Created by Simran Preet Singh Narang on 2023-10-25.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupTabBar()
+    }
+
+    private func setupViews() {
+        let summaryVC = AccountSummaryViewController()
+        let moneyVC = MoveMoneyViewController()
+        let moreVC = MoreViewController()
+
+        summaryVC.setTabBarImage(imageName: "list.dash.header.rectangle", title: "Summary")
+        moneyVC.setTabBarImage(imageName: "arrow.left.arrow.right", title: "Move Money")
+        moreVC.setTabBarImage(imageName: "ellipsis.circle", title: "More")
+
+        let summaryNC = UINavigationController(rootViewController: summaryVC)
+        let moneyNC = UINavigationController(rootViewController: moneyVC)
+        let moreNC = UINavigationController(rootViewController: moreVC)
+
+        // NOTE: Tint on Navigation Bar
+        summaryNC.navigationBar.barTintColor = appColor
+        hideNavigationBarLine(summaryNC.navigationBar)
+        
+        let tabBarList = [summaryNC, moneyNC, moreNC]
+
+        viewControllers = tabBarList
+    }
+    
+    private func setupTabBar() {
+        tabBar.tintColor = appColor
+        tabBar.isTranslucent = false
+    }
+}
+
+class AccountSummaryViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemGreen
+    }
+}
+
+class MoveMoneyViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemOrange
+    }
+}
+
+class MoreViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemPurple
+    }
+}
---
+++ Bankey/Bankey/UIViewController+Utils.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewController+Utils.swift
+//  Bankey
+//
+//  Created by Simran Preet Singh Narang on 2023-10-25.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let statusBarSize = UIApplication.shared.statusBarFrame.size
+        let frame = CGRect(origin: .zero, size: statusBarSize)
+        let statusBarView = UIView(frame: frame)
+        
+        statusBarView.backgroundColor = appColor
+        view.addSubview(statusBarView)
+    }
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+    
+    func hideNavigationBarLine(_ navigationBar: UINavigationBar) {
+        let img = UIImage()
+        navigationBar.shadowImage = img
+        navigationBar.setBackgroundImage(img, for: .default)
+        navigationBar.isTranslucent = false
+    }
+}
