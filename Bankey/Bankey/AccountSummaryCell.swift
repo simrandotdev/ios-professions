4c78130009c399250e04e04814618969b4b3c546
Simran <simran5590@gmail.com>
2023-10-26 18:01:45 -0700

    created AccountSummaryCell

---
+++ Bankey/Bankey/AccountSummaryCell.swift
@@ -0,0 +1,32 @@
+//
+//  AccountSummaryCell.swift
+//  Bankey
+//
+//  Created by Simran Preet Singh Narang on 2023-10-26.
+//
+
+import UIKit
+
+class AccountSummaryCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+extension AccountSummaryCell {
+    
+    func setup() {
+        
+    }
+    
+    func layout() {
+        
+    }
+}
